b6909d2d44e1bbd27d471cf8af8d120b7511ba34
Euiyeon Ko <rhdmldus@gamil.com>
2021-02-04 11:22:52 +0900

    숙박이용방법_pc(다시)

--- 숙박이용방법_pc/js/index.js
+++ 숙박이용방법_pc/js/index.js
@@ -1,5 +1,18 @@
+// var imgContainer = document.querySelector('#imgContainer')
+// document.querySelector('.link').addEventListener('click',function(e){
+//     imgContainer.style.display="none";
+//     e.preventDefault();
+// })
+// document.querySelector('.here').addEventListener('click',function(e){
+//     imgContainer.style.display="block"
+//     e.preventDefault();
+// })
 
-document.querySelector('.link').addEventListener('click',function(e){
-    document.querySelector('#imgContainer').style.display="none";
-    e.preventDefault();
+$('.link').click(function(e){
+    $('#imgContainer').css({display:'none'})
+    e.preventDefault()
+})
+$('.here').click(function(e){
+    $('#imgContainer').css({display:'block'})
+    e.preventDefault()
 })
